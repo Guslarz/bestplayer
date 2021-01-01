3a0928516ae7ab4464c4753754f1feb051c2c462
Guslarz <mrkaczmareks@gmail.com>
2021-01-01 19:52:35 +0100

    Add simple distance heuristic

--- src/main/java/put/ai/games/bestplayer/BestPlayer.java
+++ src/main/java/put/ai/games/bestplayer/BestPlayer.java
@@ -1,13 +1,7 @@
 package put.ai.games.bestplayer;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeoutException;
-import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
+import java.util.*;
 import java.util.function.BinaryOperator;
-import java.util.function.Predicate;
 
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
@@ -15,7 +9,7 @@ import put.ai.games.game.Player;
 
 public class BestPlayer extends Player {
 
-  private final Algorithm algorithm = new MiniMaxAlgorithm(new RandomHeuristic());
+  private final Algorithm algorithm = new MiniMaxAlgorithm(new DistanceHeuristic());
 
   @Override
   public String getName() {
@@ -73,7 +67,8 @@ public class BestPlayer extends Player {
   /**
    * Exception signaling that time specified for algorithm has passed
    */
-  private static class AlgorithmTimeoutException extends RuntimeException {}
+  private static class AlgorithmTimeoutException extends RuntimeException {
+  }
 
   /**
    * Interface of heuristic for game state evaluation
@@ -83,11 +78,11 @@ public class BestPlayer extends Player {
     /**
      * Evalulate board for given player
      *
-     * @param color color of current player
-     * @param board game board
+     * @param maximizingPlayer color maximizing player
+     * @param board            game board
      * @return score as double (higher = better)
      */
-    double getScore(Color color, Board board);
+    double getScore(Color maximizingPlayer, Board board);
   }
 
   /**
@@ -98,8 +93,93 @@ public class BestPlayer extends Player {
     private static final Random random = new Random(0xdeadbeef);
 
     @Override
-    public double getScore(Color color, Board board) {
+    public double getScore(Color maximizingPlayer, Board board) {
       return random.nextDouble();
+    }
+  }
+
+  /**
+   * Sum of squared distances to center of points
+   * <p>
+   * infinity if won
+   * -infinity if lost
+   * 0 if draw
+   */
+  private static class DistanceHeuristic implements Heuristic {
+
+    @Override
+    public double getScore(Color maximizingPlayer, Board board) {
+      Color winner = board.getWinner(maximizingPlayer);
+      if (winner != null) {
+        if (winner == maximizingPlayer) {
+          return Double.POSITIVE_INFINITY;
+        } else if (winner == Color.EMPTY) {
+          return 0;
+        } else {
+          return Double.NEGATIVE_INFINITY;
+        }
+      }
+
+      Collection<Point> playerPoints = getAllPoints(maximizingPlayer, board);
+      Collection<Point> opponentPoints = getAllPoints(
+          Player.getOpponent(maximizingPlayer), board);
+
+      return sumOfSqDistance(opponentPoints) - sumOfSqDistance(playerPoints);
+    }
+
+    private static Collection<Point> getAllPoints(Color color, Board board) {
+      Collection<Point> points = new ArrayList<>();
+      for (int i = 0, size = board.getSize(); i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+          if (board.getState(i, j) == color) {
+            points.add(new Point(i, j));
+          }
+        }
+      }
+
+      return points;
+    }
+
+    private static double sumOfSqDistance(Collection<Point> points) {
+      double x = 0;
+      double y = 0;
+      for (Point point : points) {
+        x += point.getX();
+        y += point.getY();
+      }
+      Point center = new Point(x / points.size(), y / points.size());
+
+      double sum = 0;
+      for (Point point : points) {
+        sum += center.squaredDistanceTo(point);
+      }
+
+      return sum;
+    }
+
+    private static class Point {
+
+      private final double x;
+      private final double y;
+
+      public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+      }
+
+      public double getX() {
+        return x;
+      }
+
+      public double getY() {
+        return y;
+      }
+
+      public double squaredDistanceTo(Point other) {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return dx * dx + dy * dy;
+      }
     }
   }
 
@@ -127,19 +207,17 @@ public class BestPlayer extends Player {
     }
 
     @Override
-    public abstract Move nextMove(Color color, Board board, long timeLimit);
+    public abstract Move nextMove(Color player, Board board, long timeLimit);
 
     /**
      * Get heuristic score for given game state
      *
-     * @param maximizer player maximizing score
-     * @param current player who would make move now
-     * @param board game board
+     * @param maximizingPlayer player maximizing score
+     * @param board            game board
      * @return heuristic score
      */
-    public double getHeuristicScore(Color maximizer, Color current, Board board) {
-      double multiplier = maximizer == current ? -1.0 : 1.0;
-      return multiplier * heuristic.getScore(current, board);
+    public double getHeuristicScore(Color maximizingPlayer, Board board) {
+      return heuristic.getScore(maximizingPlayer, board);
     }
   }
 
@@ -152,28 +230,28 @@ public class BestPlayer extends Player {
       super(heuristic);
     }
 
-    public Move nextMove(Color color, Board board, long timeLimit) {
-      List<Move> moves = board.getMovesFor(color);
+    public Move nextMove(Color player, Board board, long timeLimit) {
+      List<Move> moves = board.getMovesFor(player);
       if (moves.size() == 0) {
         return null;
       }
 
       return moves.stream()
-          .map(move -> evaluateMove(move, color, board))
+          .map(move -> evaluateMove(move, player, board))
           .max(new EvaluatedMoveComparator()).get().getMove();
     }
 
-    private EvaluatedMove evaluateMove(Move move, Color color, Board board) {
+    private EvaluatedMove evaluateMove(Move move, Color maximizingPlayer, Board board) {
       Board nextBoard = board.clone();
       nextBoard.doMove(move);
-      return new EvaluatedMove(getHeuristicScore(color, color, nextBoard), move);
+      return new EvaluatedMove(getHeuristicScore(maximizingPlayer, nextBoard), move);
     }
   }
 
   /**
    * Mini max algorithm with alpha beta pruning
    * and iterative deepening to prevent timeout
-   *
+   * <p>
    * Doesn't store tree
    */
   private static class MiniMaxAlgorithm extends HeuristicAlgorithm {
@@ -186,7 +264,7 @@ public class BestPlayer extends Player {
     }
 
     @Override
-    public Move nextMove(Color color, Board board, long timeLimit) {
+    public Move nextMove(Color player, Board board, long timeLimit) {
       long startTime = System.currentTimeMillis();
       long actualTimeLimit = Long.max(
           (long) (timeLimit * usableTimePercent),
@@ -198,7 +276,7 @@ public class BestPlayer extends Player {
 
       for (int depth = 1; run; ++depth) {
         try {
-          bestMove = miniMax(color, color, board,
+          bestMove = miniMax(player, player, board,
               Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY,
               0, depth, maxTime).getMove();
         } catch (AlgorithmTimeoutException ex) {
@@ -215,7 +293,7 @@ public class BestPlayer extends Player {
       return bestMove;
     }
 
-    private EvaluatedMove miniMax(Color maximizer, Color current, Board board,
+    private EvaluatedMove miniMax(Color maximizingPlayer, Color current, Board board,
                                   double alpha, double beta, int depth, int maxDepth,
                                   long maxTime) throws AlgorithmTimeoutException {
       // Throw exception if timeout
@@ -225,13 +303,13 @@ public class BestPlayer extends Player {
 
       // Evaluate board if reached maxDepth or game ended
       if (depth == maxDepth || board.getWinner(current) != null) {
-        double score = getHeuristicScore(maximizer, current, board);
+        double score = getHeuristicScore(maximizingPlayer, board);
         return new EvaluatedMove(score, null);
       }
 
       EvaluatedMove result = new EvaluatedMove();
       BinaryOperator<EvaluatedMove> better;
-      if (current == maximizer) {
+      if (current == maximizingPlayer) {
         result.setScore(Double.NEGATIVE_INFINITY);
         better = BinaryOperator.maxBy(new EvaluatedMoveComparator());
       } else {
@@ -243,13 +321,13 @@ public class BestPlayer extends Player {
       for (Move move : moves) {
         Board nextBoard = board.clone();
         nextBoard.doMove(move);
-        EvaluatedMove evaluatedMove = miniMax(maximizer, Player.getOpponent(current),
+        EvaluatedMove evaluatedMove = miniMax(maximizingPlayer, Player.getOpponent(current),
             nextBoard, alpha, beta, depth + 1, maxDepth, maxTime);
         evaluatedMove.setMove(move);
         result = better.apply(result, evaluatedMove);
         double score = result.getScore();
 
-        if (maximizer == current) {
+        if (maximizingPlayer == current) {
           if (score >= beta) {
             return result;
           }
